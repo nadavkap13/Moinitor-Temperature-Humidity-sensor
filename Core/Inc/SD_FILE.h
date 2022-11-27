b63ea69e3831e24aecdcd3b6f2701cbda03c5b1c
Your Name <you@example.com>
2022-11-27 20:43:53 +0200

    update-sd_card

---
+++ Core/Inc/SD_FILE.h
@@ -0,0 +1,14 @@
+#ifndef INC_SD_FILE_H_
+#define INC_SD_FILE_H_
+#include "main.h"
+#include "fatfs.h"
+class FILE{
+private:
+	FIL event_file;
+	FIL temperature_file;
+public:
+
+};
+
+
+#endif /* INC_SD_FILE_H_ */
